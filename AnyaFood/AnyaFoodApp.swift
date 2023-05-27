71ff94d77a70342d8c482e526a8037f274984030
horamupa <horamupa@gmail.com>
2023-05-27 18:33:36 +0300

    Initial Commit

---
+++ AnyaFood/AnyaFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyaFoodApp.swift
+//  AnyaFood
+//
+//  Created by MM on 27.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnyaFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
